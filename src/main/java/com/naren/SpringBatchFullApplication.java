5e5af8275b1937861370bb5c4d17410eb387ad4c
narendrajava1 <narendrakolli08@gmail.com>
2023-07-23 13:51:30 +0530

    initial commit

---
+++ src/main/java/com/naren/SpringBatchFullApplication.java
@@ -0,0 +1,13 @@
+package com.naren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchFullApplication.class, args);
+	}
+
+}
